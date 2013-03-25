f31c6cdc346190f978d3b73bc30e4b0bb7d55b1f
Kasper Skou Ladefoged <Kasper.ladefoged@gmail.com>
2013-03-25 10:11:05 +0100

    Added another system out line...

--- HorrorGame/src/mta13438/main.java
+++ HorrorGame/src/mta13438/main.java
@@ -8,6 +8,7 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld GitHub is working!!!");
 		System.out.println("A change in the force...");
+		System.out.println("Another change in the force for github...");
 	}
 
 }

b33b7c2f4dd778dd8f9e85304e78ffc0acb53e0e
Kasper <Kasper.ladefoged@gmail.om>
2013-04-01 18:16:13 +0200

    Extended Entity with subclass Player. Added functions in Player.java

--- HorrorGame/src/mta13438/Player.java
+++ HorrorGame/src/mta13438/Player.java
@@ -1,5 +1,30 @@
 package mta13438;
 
-public class Player {
-
+public class Player extends Entity {
+	
+	private int health;
+	
+	@Override
+	public Player() {
+		super();
+		setHealth(0);
+	}
+	@Override
+	public Player(Point pos, float speed, float angle, int health) {
+		super();
+		setHealth(health);
+	}	
+	
+	public int getHealth() {
+		return health;
+	}
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	
+	@Override
+	public String toString() {
+		return "Player [health=" + getHealth() + "speed=" + super.getSpeed() + ", angle=" + super.getAngle() + "]";
+	}
+	
 }

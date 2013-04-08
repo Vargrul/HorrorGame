a490aef2838150f9820ee5e5597ea5f8dfd68236
Kasper Skou Ladefoged <Kasper.ladefoged@gmail.com>
2013-04-08 12:01:51 +0200

    Further cleaned up

--- HorrorGame/src/mta13438/main.java
+++ HorrorGame/src/mta13438/main.java
@@ -1,10 +1,4 @@
 package mta13438;
-import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.glClear;
-
-import java.util.ArrayList;
-import org.lwjgl.opengl.Display;
-
 
 public class main {
 	public static void main(String[] args) {

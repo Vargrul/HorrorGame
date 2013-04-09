ecfdc61ca6e8379c745b6365b3a55462f6b8ac92
m-kirkeby <Khorn@gmail.com>
2013-04-09 18:26:12 +0200

    Added missing Draw functions to Trap, Wall and Water

--- HorrorGame/src/mta13438/Trap.java
+++ HorrorGame/src/mta13438/Trap.java
@@ -1,5 +1,12 @@
 package mta13438;
 
+import static org.lwjgl.opengl.GL11.GL_LINES;
+import static org.lwjgl.opengl.GL11.glBegin;
+import static org.lwjgl.opengl.GL11.glColor4f;
+import static org.lwjgl.opengl.GL11.glEnd;
+import static org.lwjgl.opengl.GL11.glLineWidth;
+import static org.lwjgl.opengl.GL11.glVertex2i;
+
 public class Trap extends Obs {
 	
 	private Sound trapKillSound = new Sound(TRAPKILLSOUND,getPos(),10,10);
@@ -10,6 +17,25 @@ public class Trap extends Obs {
 	}
 	public void collide(){
 		trapKillSound.play();
+	}
+	@Override
+	public void draw(){
+		glColor4f(0.5f, 0.4f, 0.3f, 0.2f);
+	   	 glLineWidth(1.2f);
+	   	 glBegin(GL_LINES);
+	   		 //Bottom Line
+	   		 glVertex2i((int)getPos().getX(), (int)getPos().getY());
+	   		 glVertex2i((int)(getPos().getX()+this.getDx()), (int)getPos().getY());
+	   		 //Left Line
+	   		 glVertex2i((int)getPos().getX(), (int)getPos().getY());
+	   		 glVertex2i((int)getPos().getX(), (int)(getPos().getY()+this.getDy()));
+	   		 //Top Line
+	   		 glVertex2i((int)getPos().getX(), (int)(getPos().getY()+this.getDy()));
+	   		 glVertex2i((int)(getPos().getX()+this.getDx()), (int)(getPos().getY()+this.getDy()));
+	   		 //Right Line
+	   		 glVertex2i((int)(getPos().getX()+this.getDx()), (int)(getPos().getY()+this.getDy()));
+	   		 glVertex2i((int)(getPos().getX()+this.getDx()), (int)getPos().getY());
+	   	 glEnd();
 	}
 	
 	@Override
--- HorrorGame/src/mta13438/Wall.java
+++ HorrorGame/src/mta13438/Wall.java
@@ -1,5 +1,12 @@
 package mta13438;
 
+import static org.lwjgl.opengl.GL11.GL_LINES;
+import static org.lwjgl.opengl.GL11.glBegin;
+import static org.lwjgl.opengl.GL11.glColor4f;
+import static org.lwjgl.opengl.GL11.glEnd;
+import static org.lwjgl.opengl.GL11.glLineWidth;
+import static org.lwjgl.opengl.GL11.glVertex2i;
+
 public class Wall extends Obs {
 	
 	private Sound collisionSound = new Sound(HANDSONWALL,getPos(),10,10);
@@ -12,5 +19,24 @@ public class Wall extends Obs {
 	public String toString() {
 		return "Wall [pos=" + getPos() + ", dx=" + getDx() + ", dz=" + getDz()
 				+ ", lenght=" + getDy() + ", sabins=" + getSabins() + "]";
+	}
+	@Override
+	public void draw(){
+		glColor4f(1.0f, 1.0f, 1.0f, 0.5f);
+	   	 glLineWidth(1.5f);
+	   	 glBegin(GL_LINES);
+	   		 //Bottom Line
+	   		 glVertex2i((int)getPos().getX(), (int)getPos().getY());
+	   		 glVertex2i((int)(getPos().getX()+this.getDx()), (int)getPos().getY());
+	   		 //Left Line
+	   		 glVertex2i((int)getPos().getX(), (int)getPos().getY());
+	   		 glVertex2i((int)getPos().getX(), (int)(getPos().getY()+this.getDy()));
+	   		 //Top Line
+	   		 glVertex2i((int)getPos().getX(), (int)(getPos().getY()+this.getDy()));
+	   		 glVertex2i((int)(getPos().getX()+this.getDx()), (int)(getPos().getY()+this.getDy()));
+	   		 //Right Line
+	   		 glVertex2i((int)(getPos().getX()+this.getDx()), (int)(getPos().getY()+this.getDy()));
+	   		 glVertex2i((int)(getPos().getX()+this.getDx()), (int)getPos().getY());
+	   	 glEnd();
 	}
 }

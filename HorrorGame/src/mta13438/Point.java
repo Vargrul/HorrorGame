7dd71bad3d14afb0672adcd5f12327ee5d6e1087
Kasper Skou Ladefoged <Kasper.ladefoged@gmail.com>
2013-04-18 18:31:01 +0200

    Added even more comments

--- HorrorGame/src/mta13438/Point.java
+++ HorrorGame/src/mta13438/Point.java
@@ -3,38 +3,36 @@ package mta13438;
 public class Point {
 	private float x,y,z;
 	
+	//no args constructor
 	public Point(){
 		setX(0);
 		setY(0);
 		setZ(0);
 	}
 	
+	//Constructor
 	public Point(float x, float y,float z){
 		setX(x);
 		setY(y);
 		setZ(z);
 	}
 
+	//getters and setters
 	public float getX() {
 		return x;
 	}
-
 	public void setX(float x) {
 		this.x = x;
 	}
-
 	public float getY() {
 		return y;
 	}
-
 	public void setY(float y) {
 		this.y = y;
 	}
-
 	public float getZ() {
 		return z;
 	}
-
 	public void setZ(float z) {
 		this.z = z;
 	}

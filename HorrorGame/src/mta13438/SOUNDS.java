ceacb2c9889b87e0f753b492854426132be602ac
Kasper Skou Ladefoged <Kasper.ladefoged@gmail.com>
2013-05-14 09:40:02 +0200

    More path shits and gizzle

--- HorrorGame/src/mta13438/SOUNDS.java
+++ HorrorGame/src/mta13438/SOUNDS.java
@@ -3,15 +3,15 @@ package mta13438;
 public  enum SOUNDS {
 	//Add sounds and path 
 	MENU_MUSIC("assets/Music/MenuBackground_1.wav"),
-	DOOR("/assets/soundEffects/door.wav"),
-	FOOTSTEP_STONE("/assets/soundEffects/footstep_stone.wav"),
-	FOOTSTEP_WATER("/assets/soundEffects/footstep_water.wav"),
-	GUILLOTINE_TRAP("/assets/soundEffects/Guillotine_trap.wav"),
-	MONSTER1("/assets/soundEffects/monster1.wav"),
-	RAT("/assets/soundEffects/Rat.wav"),
-	ROTER_TRAP("/assets/soundEffects/Roter_trap.wav"),
-	WATERDROP1("/assets/soundEffects/waterdrop1.wav"),
-	WATERDROP2("/assets/soundEffects/waterdrop2.wav"),
+	DOOR("assets/soundEffects/door.wav"),
+	FOOTSTEP_STONE("assets/soundEffects/footstep_stone.wav"),
+	FOOTSTEP_WATER("assets/soundEffects/footstep_water.wav"),
+	GUILLOTINE_TRAP("assets/soundEffects/Guillotine_trap.wav"),
+	MONSTER1("assets/soundEffects/monster1.wav"),
+	RAT("assets/soundEffects/Rat.wav"),
+	ROTER_TRAP("assets/soundEffects/Roter_trap.wav"),
+	WATERDROP1("assets/soundEffects/waterdrop1.wav"),
+	WATERDROP2("assets/soundEffects/waterdrop2.wav"),
 	;
 	
 	//desc for description

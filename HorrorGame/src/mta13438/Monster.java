6ebaadd8516433907b07e52d14c7c851691eb3e2
Jeppe Bentzen <bentzen.jeppe@gmail.com>
2013-05-15 15:51:15 +0200

    sasdasdsadasdasd

--- HorrorGame/src/mta13438/Monster.java
+++ HorrorGame/src/mta13438/Monster.java
@@ -23,7 +23,7 @@ public class Monster extends Obs {
 	}
 	
 	public void collision(Player player, Level level, int currentRoom){
-		System.out.println("Collision with monster");
+		//System.out.println("Collision with monster");
 		level.setMonsterDeath(true);
 	}
 	@Override

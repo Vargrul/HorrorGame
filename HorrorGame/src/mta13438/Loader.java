d73d72a8350c754e2d68cef345b9852ff8a7ddae
m-kirkeby <Khorn@gmail.com>
2013-05-16 21:37:47 +0200

    OMG FINALLY FUCKING DONE FOR THE 3RD FUCKING TIME. NO MORE REDONING THE LOFI TEST!!!! also huehuehuehue

--- HorrorGame/src/mta13438/Loader.java
+++ HorrorGame/src/mta13438/Loader.java
@@ -27,7 +27,8 @@ public class Loader {
 	private static int keyCD = 0;
 	private static int[] xArray;
 	private static int[] yArray;
-	private static ArrayList testSounds = new ArrayList<Sound>(); 
+	private static ArrayList<Sound> testSounds = new ArrayList<Sound>(); 
+	private static ArrayList<Sound> soundOrder = new ArrayList<Sound>(); 
 	private static ArrayList visualPoints = new ArrayList<Trap>();
 	private static int randomNum;
 	private static int soundNr;
@@ -36,6 +37,8 @@ public class Loader {
 	private static boolean testDone = false;
 	private static double bestDistance = 0;
 	private static double yourDistance = 0;
+	private static ArrayList<Integer> soundNumbers = new ArrayList<Integer>();
+	private static boolean displayResult = false;
 	
 	private static Sound walkSound = new Sound(SOUNDS.FOOTSTEP_STONE, player.getPos(), true, 0.5f);
 	private static Sound walkWaterSound = new Sound(SOUNDS.FOOTSTEP_WATER, player.getPos(), true, 0.5f);
@@ -77,9 +80,13 @@ public class Loader {
 		tutorialLevel.getRoomList().get(0).addObsList(trap5);
 		tutorialLevel.getRoomList().get(0).addObsList(trap6);
 		
-		//Locate memory for arrays
-		xArray = new int[6];
-		yArray = new int[6];
+		//Add sound numbers to arrayList
+		soundNumbers.add(1);
+		soundNumbers.add(2);
+		soundNumbers.add(3);
+		soundNumbers.add(4);
+		soundNumbers.add(5);
+		soundNumbers.add(6);
 		
 		//Adding sounds to ArrayList
 		testSounds.add(sound1);
@@ -89,19 +96,11 @@ public class Loader {
 		testSounds.add(sound5);
 		testSounds.add(sound6);
 		
+		soundOrder.clear();
+		
 		//Choose(Random) first sound to be played
-		randomNum = 0 + (int)(Math.random()*6);
-		soundNr = randomNum+1;
-		
-		bestDistance += getDistance(sound1.getPos(), sound2.getPos());
-		bestDistance += getDistance(sound2.getPos(), sound3.getPos());
-		bestDistance += getDistance(sound3.getPos(), sound4.getPos());
-		bestDistance += getDistance(sound4.getPos(), sound5.getPos());
-		bestDistance += getDistance(sound5.getPos(), sound6.getPos());
-		bestDistance += getDistance(sound6.getPos(), sound1.getPos());
-		System.out.println("Best possible distance: " + bestDistance);
-		
-		
+		randomNum = 0 + (int)(Math.random()*soundNumbers.size());
+		soundNr = soundNumbers.get(randomNum);
 		
 		switch(soundNr){
 		case(1): System.out.println("Player start at pos 6");
@@ -124,10 +123,12 @@ public class Loader {
 		break;
 		}
 		
-		System.out.println("Start. Playing Sound " + soundNr + ".");
+		System.out.println("Playing Sound " + soundNr + ".");
 		currentSound = (Sound) testSounds.get(soundNr-1);
+		soundOrder.add(currentSound);
 		currentSound.play();
 		soundPlayed++;
+		System.out.println("Number of sounds left: " + soundNumbers.size());
 		
 
 		
@@ -171,113 +172,160 @@ public class Loader {
 						switch(i){
 						case(1):
 							if(soundNr == 1){
-								System.out.println("Collision. Playing Sound 2.");
-								soundNr = 2;
-								soundPlayed++;
-								sound1.stop();
-								sound2.play();
+								//Stop the current sound
+								currentSound.stop();
+								//Remove the last sound from the sound pool
+								if(soundNumbers.size()!=1){
+									soundNumbers.remove(randomNum);
+									//Randomize next sound
+									randomNum = 0 + (int)(Math.random()*soundNumbers.size());
+									soundNr = soundNumbers.get(randomNum);
+									//Print Message
+									System.out.println("Collision. Playing Sound " + soundNr + ".");
+									//Set the new current sound to the randomized number from the pool
+									currentSound = getNewSound();
+									//Add the new current sound to the order arraylist
+									soundOrder.add(currentSound);
+									//Play the new current sound
+									currentSound.play();
+									//Increase number of sounds played
+									soundPlayed++;
+									System.out.println("Number of sounds left: " + soundNumbers.size());
+								}
+								
 							}
 						break;
 						case(2):
 							if(soundNr == 2){
-								System.out.println("Collision. Playing Sound 3.");
-								soundNr = 3;
-								soundPlayed++;
-								sound2.stop();
-								sound3.play();
+								//Stop the current sound
+								currentSound.stop();
+								//Remove the last sound from the sound pool
+								if(soundNumbers.size()!=1){
+									soundNumbers.remove(randomNum);
+									//Randomize next sound
+									randomNum = 0 + (int)(Math.random()*soundNumbers.size());
+									soundNr = soundNumbers.get(randomNum);
+									//Print Message
+									System.out.println("Collision. Playing Sound " + soundNr + ".");
+									//Set the new current sound to the randomized number from the pool
+									currentSound = getNewSound();
+									//Add the new current sound to the order arraylist
+									soundOrder.add(currentSound);
+									//Play the new current sound
+									currentSound.play();
+									//Increase number of sounds played
+									soundPlayed++;
+									System.out.println("Number of sounds left: " + soundNumbers.size());
+								}
 							}
 						break;
 						case(3):
 							if(soundNr == 3){
-								System.out.println("Collision. Playing Sound 4.");
-								soundNr = 4;
-								soundPlayed++;
-								sound3.stop();
-								sound4.play();
+								//Stop the current sound
+								currentSound.stop();
+								//Remove the last sound from the sound pool
+								if(soundNumbers.size()!=1){
+									soundNumbers.remove(randomNum);
+									//Randomize next sound
+									randomNum = 0 + (int)(Math.random()*soundNumbers.size());
+									soundNr = soundNumbers.get(randomNum);
+									//Print Message
+									System.out.println("Collision. Playing Sound " + soundNr + ".");
+									//Set the new current sound to the randomized number from the pool
+									currentSound = getNewSound();
+									//Add the new current sound to the order arraylist
+									soundOrder.add(currentSound);
+									//Play the new current sound
+									currentSound.play();
+									//Increase number of sounds played
+									soundPlayed++;
+									System.out.println("Number of sounds left: " + soundNumbers.size());
+								}
 							}
 						break;
 						case(4):
 							if(soundNr == 4){
-								System.out.println("Collision. Playing Sound 5.");
-								soundNr = 5;
-								soundPlayed++;
-								sound4.stop();
-								sound5.play();
+								//Stop the current sound
+								currentSound.stop();
+								//Remove the last sound from the sound pool
+								if(soundNumbers.size()!=1){
+									soundNumbers.remove(randomNum);
+									//Randomize next sound
+									randomNum = 0 + (int)(Math.random()*soundNumbers.size());
+									soundNr = soundNumbers.get(randomNum);
+									//Print Message
+									System.out.println("Collision. Playing Sound " + soundNr + ".");
+									//Set the new current sound to the randomized number from the pool
+									currentSound = getNewSound();
+									//Add the new current sound to the order arraylist
+									soundOrder.add(currentSound);
+									//Play the new current sound
+									currentSound.play();
+									//Increase number of sounds played
+									soundPlayed++;
+									System.out.println("Number of sounds left: " + soundNumbers.size());
+								}
 							}
 						break;
 						case(5):
 							if(soundNr == 5){
-								System.out.println("Collision. Playing Sound 6.");
-								soundNr = 6;
-								soundPlayed++;
-								sound5.stop();
-								sound6.play();
+								//Stop the current sound
+								currentSound.stop();
+								//Remove the last sound from the sound pool
+								if(soundNumbers.size()!=1){
+									soundNumbers.remove(randomNum);
+									//Randomize next sound
+									randomNum = 0 + (int)(Math.random()*soundNumbers.size());
+									soundNr = soundNumbers.get(randomNum);
+									//Print Message
+									System.out.println("Collision. Playing Sound " + soundNr + ".");
+									//Set the new current sound to the randomized number from the pool
+									currentSound = getNewSound();
+									//Add the new current sound to the order arraylist
+									soundOrder.add(currentSound);
+									//Play the new current sound
+									currentSound.play();
+									//Increase number of sounds played
+									soundPlayed++;
+									System.out.println("Number of sounds left: " + soundNumbers.size());
+								}
 							}
 						break;
 						case(6):
 							if(soundNr == 6){
-								System.out.println("Collision. Playing Sound 1.");
-								soundNr = 1;
-								soundPlayed++;
-								sound6.stop();
-								sound1.play();
+								//Stop the current sound
+								currentSound.stop();
+								//Remove the last sound from the sound pool
+								if(soundNumbers.size()!=1){
+									soundNumbers.remove(randomNum);
+									//Randomize next sound
+									randomNum = 0 + (int)(Math.random()*soundNumbers.size());
+									soundNr = soundNumbers.get(randomNum);
+									//Print Message
+									System.out.println("Collision. Playing Sound " + soundNr + ".");
+									//Set the new current sound to the randomized number from the pool
+									currentSound = getNewSound();
+									//Add the new current sound to the order arraylist
+									soundOrder.add(currentSound);
+									//Play the new current sound
+									currentSound.play();
+									//Increase number of sounds played
+									soundPlayed++;
+									System.out.println("Number of sounds left: " + soundNumbers.size());
+								}
 							}
 						break;
 						}
-						if(soundPlayed==7){
-							if(sound1.isPlaying==true){
-								sound1.stop();
+						
+						if(soundPlayed==6){
+							if(currentSound.isPlaying == false && displayResult == false ){
+								displayResult = true;
 								testDone = true;
-								System.out.println("Test is over");
-								System.out.println("That was 6 sound played");
-								getFinalSteps();
+								currentSound.stop();
+								System.out.println("Test is Done");
+								getFinalDistance();
 								getFinalTime();
 							}
-							if(sound2.isPlaying==true){
-								sound2.stop();
-								testDone = true;
-								System.out.println("Test is over");
-								System.out.println("That was 6 sound played");
-								getFinalSteps();
-								getFinalTime();
-							}
-							if(sound3.isPlaying==true){
-								sound3.stop();
-								testDone = true;
-								System.out.println("Test is over");
-								System.out.println("That was 6 sound played");
-								getFinalSteps();
-								getFinalTime();
-							}
-							if(sound4.isPlaying==true){
-								sound4.stop();
-								testDone = true;
-								System.out.println("Test is over");
-								System.out.println("That was 6 sound played");
-								getFinalSteps();
-								getFinalTime();
-							}
-							if(sound5.isPlaying==true){
-								sound5.stop();
-								testDone = true;
-								System.out.println("Test is over");
-								System.out.println("That was 6 sound played");
-								getFinalSteps();
-								getFinalTime();
-							}
-							if(sound6.isPlaying==true){
-								sound6.stop();
-								testDone = true;
-								System.out.println("Test is over");
-								System.out.println("That was 6 sound played");
-								getFinalSteps();
-								getFinalTime();
-							}
-							
-							//for(int j = 0; j<pathPoints.size(); j++){
-							//	pathPoints.get(j).draw();
-							//	System.out.println("Drawing Points");
-							//}
 						}
 						
 						}
@@ -334,18 +382,46 @@ public class Loader {
 		time++;
 	}
 
+	private static Sound getNewSound() {
+		if(soundNr ==1){
+			return sound1;
+		}
+		if(soundNr ==2){
+			return sound2;
+		}
+		if(soundNr ==3){
+			return sound3;
+		}
+		if(soundNr ==4){
+			return sound4;
+		}
+		if(soundNr ==5){
+			return sound5;
+		}
+		if(soundNr ==6){
+			return sound6;
+		}
+		return currentSound;
+	}
 	private static void getFinalTime() {
 		System.out.println("Final time: " + time/61);
 	}
 	
-	private static void getFinalSteps() {
+	private static void getFinalDistance() {
+		
+		bestDistance += getDistance(soundOrder.get(0).getPos(), soundOrder.get(1).getPos());
+		bestDistance += getDistance(soundOrder.get(1).getPos(), soundOrder.get(2).getPos());
+		bestDistance += getDistance(soundOrder.get(2).getPos(), soundOrder.get(3).getPos());
+		bestDistance += getDistance(soundOrder.get(3).getPos(), soundOrder.get(4).getPos());
+		bestDistance += getDistance(soundOrder.get(4).getPos(), soundOrder.get(5).getPos());
+		bestDistance += getDistance(soundOrder.get(5).getPos(), soundOrder.get(0).getPos());
+		System.out.println("Best possible distance: " + bestDistance);
 		
 		for(int i = 0;i<pathPoints.size()-1; i++){
 			yourDistance += getDistance(pathPoints.get(i), pathPoints.get(i+1));
 		}
 		
 		System.out.println("Your distance: " + yourDistance);
-		System.out.println("Total distance: " + pathPoints.size());
 	}
 	
 	
@@ -372,7 +448,7 @@ public class Loader {
 			enterPressed = true;
 			System.out.println("Test is over");
 			System.out.println("That was 6 sound played");
-			getFinalSteps();
+			
 			getFinalTime();
 				
 		}	
